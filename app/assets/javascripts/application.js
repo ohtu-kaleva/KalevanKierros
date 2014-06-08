8ccd14328f1794f26f288d04eb4e14ad2c8d23c8
joentaus <joentaus@cs.helsinki.fi>
2014-06-08 13:32:14 +0300

    Fiksi WP8:lle

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,12 +16,3 @@
 //= require_tree .
 //= require bootstrap
 
-if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
-    var msViewportStyle = document.createElement('style')
-    msViewportStyle.appendChild(
-        document.createTextNode(
-            '@-ms-viewport{width:auto!important}'
-        )
-    )
-    document.querySelector('head').appendChild(msViewportStyle)
-}
