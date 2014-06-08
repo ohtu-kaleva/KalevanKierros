66a464b13d12162898b4e6e1f803556b1d5cd831
mkorento <korento.mika@gmail.com>
2014-06-08 15:02:00 +0300

    Vaihda event_attributeen vaihtoehtojen erotusmerkiksi ';'

--- app/models/event_attribute.rb
+++ app/models/event_attribute.rb
@@ -1,7 +1,7 @@
 class EventAttribute < ActiveRecord::Base
   belongs_to :event
 
-  def get_options_to_form(values)
-    values.split(':')
+  def get_options_to_form
+    attribute_value.split(';')
   end
 end
