203d4bf489b8b75b8debc12ed8af8ce84444124f
mkorento <korento.mika@gmail.com>
2014-06-08 15:41:37 +0300

    Lisää validointeja

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,15 @@
 class Event < ActiveRecord::Base
   has_many :event_attributes
+
+  validates :name, :start_date, :end_date, presence: true
+  validates :name, uniqueness: true
+  validate :validate_end_date
+
+  def validate_end_date
+    if start_date && end_date
+      return if end_date >= Date.today && end_date >= start_date
+    end
+
+    errors.add(:end_date, 'is invalid')
+  end
 end
--- app/models/event_attribute.rb
+++ app/models/event_attribute.rb
@@ -1,7 +1,16 @@
 class EventAttribute < ActiveRecord::Base
   belongs_to :event
+  validates :attribute_type, :name, :attribute_label, presence: true
+  validate :attribute_type_allowed
 
   def get_options_to_form
     attribute_value.split(';')
+  end
+
+  def attribute_type_allowed
+    return if attribute_type.in? ['radio_button', 'check_box', 'text_field',
+                                  'select', 'plain_text']
+
+    errors.add(:attribute_type, 'is invalid')
   end
 end
