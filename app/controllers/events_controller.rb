8fd06dd5365d5ea469916dee983635cdf93a0288
mkorento <korento.mika@gmail.com>
2014-06-23 05:29:02 +0300

    Lisää avoin ilmoittautuminen

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -57,6 +57,6 @@ class EventsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
-      params.require(:event).permit(:name, :start_date, :end_date, :description)
+      params.require(:event).permit(:name, :start_date, :end_date, :description, :open)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140616133234) do
+ActiveRecord::Schema.define(version: 20140622192708) do
 
   create_table "enrollment_data", force: true do |t|
     t.integer  "enrollment_id"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20140616133234) do
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "open",        default: false, null: false
   end
 
   create_table "groups", force: true do |t|
