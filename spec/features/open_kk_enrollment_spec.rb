dc96495e51fd0de8f4b7c4b20ee97d66e0e4524f
mkorento <korento.mika@gmail.com>
2014-07-02 15:01:19 +0300

    Lisää testi

--- spec/features/open_kk_enrollment_spec.rb
+++ spec/features/open_kk_enrollment_spec.rb
@@ -10,16 +10,40 @@ feature 'Opening enrollment' do
     sign_in(username: 'Tyhjis', password: 'Salainen1')
   end
 
-  scenario 'User cannot enroll if closed' do
-    visit new_kk_enrollment_path(user.id)
-    expect(page).to have_content 'Kierrokselle ei voi ilmoittautua'
+  context 'Closed enrollment' do
+    scenario 'User tries to enroll' do
+      click_link 'Esittely'
+      click_link 'Ilmoittautuminen'
+      expect(page).not_to have_button 'Ilmoittaudu kierrokselle'
+
+      visit new_kk_enrollment_path(user.id)
+      expect(page).to have_content 'Kierrokselle ei voi ilmoittautua'
+    end
+
+    scenario 'Admin opens enrollment' do
+      click_link 'Kierroksen hallinta'
+      click_button 'Avaa ilmoittautuminen'
+      expect(current_url).to eq(admin_kk_enrollment_url)
+      expect(page).to have_content 'Kierrosilmoittautuminen avattu'
+
+      visit new_kk_enrollment_path(user.id)
+      expect(page).to have_content 'Ilmoittautuminen Kalevan Kierrokselle'
+    end
   end
 
-  scenario 'Admin can open enrollment' do
-    click_link 'Kierroksen hallinta'
-    click_button 'Avaa ilmoittautuminen'
-    expect(current_url).to eq(admin_kk_enrollment_url)
-    expect(page).to have_content 'Kierrosilmoittautuminen avattu'
+  context 'Open enrollment' do
+    before :each do
+      click_link 'Kierroksen hallinta'
+      click_button 'Avaa ilmoittautuminen'
+    end
 
+    scenario 'User creates new enrollment' do
+      click_link 'Esittely'
+      click_link 'Ilmoittautuminen'
+      click_button 'Ilmoittaudu kierrokselle'
+      click_button 'Ilmoittaudu kierrokselle'
+      expect(page).to have_content 'Ilmoittautuminen onnistui'
+      expect(page).not_to have_content 'Kierrokselle ei voi ilmoittautua'
+    end
   end
 end
