7379ca866486ab737b8e338cf379eac7cb2c7e9f
mkorento <korento.mika@gmail.com>
2014-07-27 23:31:13 +0300

    Testaa sarjan määritys

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -53,4 +53,88 @@ describe User do
       expect(data.empty?).to be(true)
     end
   end
+
+  context 'returns correct series when transitioning from' do
+    let!(:user) { FactoryGirl.create :user, password:"S4lainen", password_confirmation:"S4lainen"}
+
+    it 'MAL22 to M' do
+      user.birth_date = Date.today-21.years
+      serie = user.define_series
+      expect(serie).to eq('MAL22')
+
+      user.birth_date = Date.today-22.years
+      serie = user.define_series
+      expect(serie).to eq('M')
+    end
+
+    it 'M to M40' do
+      user.birth_date = Date.today-39.years
+      serie = user.define_series
+      expect(serie).to eq('M')
+
+      user.birth_date = Date.today-40.years
+      serie = user.define_series
+      expect(serie).to eq('M40')
+    end
+
+    it 'M40 to M50' do
+      user.birth_date = Date.today-49.years
+      serie = user.define_series
+      expect(serie).to eq('M40')
+
+      user.birth_date = Date.today-50.years
+      serie = user.define_series
+      expect(serie).to eq('M50')
+    end
+
+    it 'M50 to M55' do
+      user.birth_date = Date.today-54.years
+      serie = user.define_series
+      expect(serie).to eq('M50')
+
+      user.birth_date = Date.today-55.years
+      serie = user.define_series
+      expect(serie).to eq('M55')
+    end
+
+    it 'M55 to M60' do
+      user.birth_date = Date.today-59.years
+      serie = user.define_series
+      expect(serie).to eq('M55')
+
+      user.birth_date = Date.today-60.years
+      serie = user.define_series
+      expect(serie).to eq('M60')
+    end
+
+    it 'M60 to M65' do
+      user.birth_date = Date.today-64.years
+      serie = user.define_series
+      expect(serie).to eq('M60')
+
+      user.birth_date = Date.today-65.years
+      serie = user.define_series
+      expect(serie).to eq('M65')
+    end
+
+    it 'M65 to M70' do
+      user.birth_date = Date.today-69.years
+      serie = user.define_series
+      expect(serie).to eq('M65')
+
+      user.birth_date = Date.today-70.years
+      serie = user.define_series
+      expect(serie).to eq('M70')
+    end
+
+    it 'M70 to M75' do
+      user.birth_date = Date.today-74.years
+      serie = user.define_series
+      expect(serie).to eq('M70')
+
+      user.birth_date = Date.today-75.years
+      serie = user.define_series
+      expect(serie).to eq('M75')
+    end
+  end
 end
