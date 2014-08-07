a630d2b2237c6fa0627ded32a9ac8cc5228a79c6
mkorento <korento.mika@gmail.com>
2014-08-07 17:18:19 +0300

    Lisää e-mail testit

---
+++ spec/mailers/user_mailer_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe UserMailer do
+  let!(:user) { FactoryGirl.create :user, :admin, password: 'Salainen1',
+                                   email: 'testmail@test.com',
+                                   password_confirmation: 'Salainen1' }
+  let!(:email) { UserMailer.registration_activation_email(user).deliver }
+
+  it 'sends mail' do
+    expect(ActionMailer::Base.deliveries.empty?).to eq(false)
+  end
+
+  it 'has receiver set correctly' do
+    expect(email.to).to eq([user.email])
+  end
+
+  it 'has sender set correctly' do
+    expect(email.from).to eq([ENV['EMAIL_ADDRESS']])
+  end
+
+  it 'has subject set correctly' do
+    expect(email.subject).to eq('Kalevan Kierros - Vahvista rekisteröityminen')
+  end
+
+  it 'has link to registration' do
+    expect(email.body.encoded).to match("http://0.0.0.0:3000/users/#{user.id}/activate")
+  end
+end
