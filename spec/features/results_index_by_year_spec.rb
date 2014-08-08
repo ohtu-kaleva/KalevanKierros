337667909f7d140ae067eb3c3afbad58f148678d
Lauri Vuojola <lauri.vuojola@helsinki.fi>
2014-08-08 14:52:57 +0300

    korjattu tulosten testeja

--- spec/features/results_index_by_year_spec.rb
+++ spec/features/results_index_by_year_spec.rb
@@ -7,21 +7,21 @@ feature 'Show results by year' do
 
   context "Selecting existing year" do
     scenario "shows the correct year (2014)" do
-      visit index_by_year_path(2014)
+      visit index_by_year_path(2014, "all", "all", "all")
       expect(page).to have_content('Vuoden 2014 tulokset')
       expect(page).to have_content('Kristian Hansson')
     end
     scenario "shows the correct year (2013)" do
-      visit index_by_year_path(2013)
+      visit index_by_year_path(2013, "all", "all", "all")
       expect(page).to have_content('Vuoden 2013 tulokset')
       expect(page).to have_content('Kristian Hansson')
     end
   end
 
   context "Selecting nonexisting year" do
-    scenario "redirects to root page" do
-      visit index_by_year_path(1300)
-      current_path.should eq('/')
+    scenario "redirects to results index page" do
+      visit index_by_year_path(1300, "all", "all", "all")
+      current_path.should eq('/results')
     end
   end
 end
