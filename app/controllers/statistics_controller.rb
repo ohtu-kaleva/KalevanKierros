10cc9e8b6ea0f3e7bd7cd2f969c9e8f740ad7536
mkorento <korento.mika@gmail.com>
2014-08-15 16:19:57 +0300

    Lisää kontrollerimetodi tilastoon liittämiseen

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -13,6 +13,24 @@ class StatisticsController < ApplicationController
     end
   end
 
+  def join_user_to_existing_statistic
+    @user = User.find_by id: params[:user_id]
+    @statistic = Statistic.find_by params[:statistic_id]
+
+    if @user && @statistic
+      @user.statistic.destroy unless @user.statistic.nil?
+
+      @user.statistic = @statistic
+      if @user.save
+        redirect_to :index, flash[:success] = 'Käyttäjä liitetty tilastoon'
+      else
+        redirect_to :back, flash[:error] = 'Käyttäjän liittäminen tilastoon ei onnistunut'
+      end
+    else
+      redirect_to :root
+    end
+  end
+
   # GET /statistics/1/edit
   def edit
   end
