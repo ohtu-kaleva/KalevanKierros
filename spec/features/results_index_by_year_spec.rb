5cdef8de37e98ad4a4cd1207b40d35ccd24bbeed
Lauri Vuojola <lauri.vuojola@helsinki.fi>
2014-08-20 21:35:10 +0300

    feature testit lajituloksille

--- spec/features/results_index_by_year_spec.rb
+++ spec/features/results_index_by_year_spec.rb
@@ -24,4 +24,48 @@ feature 'Show results by year' do
       current_path.should eq('/results')
     end
   end
+
+  context "Selecting  year" do
+    scenario "Shows the existing skating results for a correct year (2014)" do
+      visit index_by_year_path(2014, "all", "all", "skating")
+      expect(page).to have_content('Kristian Hansson')
+      expect(page).to have_content('Loppuaika')
+      expect(page).to have_content('01:03:43')
+    end
+
+    scenario "Shows the existing skiing results for a correct year (2014)" do
+      visit index_by_year_path(2014, "all", "all", "skiing")
+      expect(page).to have_content('Kristian Hansson')
+      expect(page).to have_content('Loppuaika')
+      expect(page).to have_content('02:46:29')
+    end
+
+    scenario "Shows the existing running results for a correct year (2014)" do
+      visit index_by_year_path(2014, "all", "all", "marathon")
+      expect(page).to have_content('Kristian Hansson')
+      expect(page).to have_content('Loppuaika')
+      expect(page).to have_content('03:14:46')
+    end
+
+    scenario "Shows the existing rowing results for a correct year (2014)" do
+      visit index_by_year_path(2014, "all", "all", "rowing")
+      expect(page).to have_content('Kristian Hansson')
+      expect(page).to have_content('Loppuaika')
+      expect(page).to have_content('02:59:41')
+    end
+
+    scenario "Shows the existing cycling results for a correct year (2014)" do
+      visit index_by_year_path(2014, "all", "all", "cycling")
+      expect(page).to have_content('Kristian Hansson')
+      expect(page).to have_content('Loppuaika')
+      expect(page).to have_content('01:50:22')
+    end
+
+    scenario "Shows the existing orienteering results for a correct year (2014)" do
+      visit index_by_year_path(2014, "all", "all", "orienteering")
+      expect(page).to have_content('Kristian Hansson')
+      expect(page).to have_content('Loppuaika')
+      expect(page).to have_content('01:26:07')
+    end
+  end
 end
