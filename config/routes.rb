266ea023e469b18569dd75b8265955a038f04fc2
mkorento <korento.mika@gmail.com>
2014-08-24 23:24:47 +0300

    Lisää tilastopäivitys ylläpitäjän sivulle

--- config/routes.rb
+++ config/routes.rb
@@ -74,6 +74,7 @@ Rails.application.routes.draw do
   get 'users/:id/statistic', to: 'statistics#join', as: 'join_statistic'
   put 'users/:id/statistic', to: 'statistics#join_user_to_existing_statistic'
   post 'statistics/update/:year', to: 'statistics#update_statistics', as: 'update_statistics'
+  post 'statistics/update', to: 'statistics#update_statistics'
   get '*path' => redirect('/')
 
   # The priority is based upon order of creation: first created -> highest priority.
