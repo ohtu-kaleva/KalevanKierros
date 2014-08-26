3a42cbd335e877d32caf6cd2aa9d5afd95c929c2
joentaus <joentaus@cs.helsinki.fi>
2014-08-26 14:31:03 +0300

    default-sorttaus statisticseihin sukunimen mukaan

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -12,13 +12,13 @@ class StatisticsController < ApplicationController
     if params[:sort] == 'points'
       @statistics = Statistic.all.order('total_events_completed desc').paginate(page: statistic_params[:page], per_page: 100)
     else
-      @statistics = Statistic.all.paginate(page: statistic_params[:page], per_page: 100)
+      @statistics = Statistic.all.order('last_name').paginate(page: statistic_params[:page], per_page: 100)
     end
   end
 
   # GET /statistics/search
   def index_filterable
-    @statistics = Statistic.all
+    @statistics = Statistic.all.order('last_name')
   end
   # GET /statistics/1
   # GET /statistics/1.json
