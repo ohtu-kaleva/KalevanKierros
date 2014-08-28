6ac5bc27e983367e89265756aa2da8cae5a4a884
mkorento <korento.mika@gmail.com>
2014-08-28 22:39:30 +0300

    Lisää testi uutisarkistoille

--- spec/features/uutiset_page_spec.rb
+++ spec/features/uutiset_page_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 include SigninHelper
 
-feature 'Create new news item' do
+feature 'News page' do
   let!(:user) { FactoryGirl.create :user, :admin, password: 'Salainen1',
       password_confirmation: 'Salainen1' }
   before :each do
@@ -23,8 +23,36 @@ feature 'Create new news item' do
     est laborum.'
 
     click_button 'Tallenna'
-    expect(page).to have_content('Uutinen luotu.')
-    expect(page).to have_content('Lorem ipsum dolor sit amet')
-    expect(page).to have_content('est laborum.')
+    expect(page).to have_content 'Uutinen luotu.'
+    expect(page).to have_content 'Lorem ipsum dolor sit amet'
+    expect(page).to have_content 'est laborum.'
+  end
+
+  context 'with 4 news items' do
+    before :each do
+      Uutiset.create title: 'title1', body: 'testbody1', created_at: DateTime.new(2013, 12, 31, 23, 59, 59)
+      Uutiset.create title: 'title2', body: 'testbody2', created_at: DateTime.new(2014, 1, 1, 0, 0, 0)
+      Uutiset.create title: 'title3', body: 'testbody3', created_at: DateTime.new(2014, 12, 31, 23, 59, 59)
+      Uutiset.create title: 'title4', body: 'testbody4', created_at: DateTime.new(2015, 1, 1, 0, 0, 0)
+    end
+
+    scenario 'Admin browses news by year' do
+      visit archive_by_year_path('2014')
+      expect(page).to have_content 'Uutisarkisto 2014'
+      expect(page).to have_content 'title2'
+      expect(page).to have_content 'title3'
+
+      expect(page).not_to have_content 'title1'
+      expect(page).not_to have_content 'title4'
+
+      click_link 'title2'
+      expect(page).to have_content 'title2'
+      expect(page).to have_content 'testbody2'
+    end
+
+    scenario 'Admin tries to browse with incorrect year' do
+      visit archive_by_year_path('123d')
+      current_path.should == '/'
+    end
   end
 end
