441886a6fec9dc3d688b043b1040dba50d7ea617
Kristian Hansson <Tyhjis@Kristians-MacBook-Pro.local>
2015-04-17 14:27:38 +0300

    salasanan palautus aloitettu

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,5 +1,7 @@
 class PasswordResetsController < ApplicationController
 
+  before_action :set_user_or_redirect, only: [ :edit, :update ]
+
   def new
   end
 
@@ -7,11 +9,31 @@ class PasswordResetsController < ApplicationController
   end
 
   def create
-    u = User.find_by username: params[:username], email: params[:email]
-    puts u.full_name
-    redirect_to :back
+    user = User.find_by username: params[:username], email: params[:email]
+    if user
+      user.generate_reset_token
+      user.set_reset_time
+      puts "#{user.reset_sent_at}"
+      UserMailer.reset_password_email(user).deliver
+      redirect_to :root, flash: { success: "Antamaasi sähköpostiosoitteeseen: #{user.email} on lähetetty viesti. Seuraa siinä näkyviä ohjeita." } and return
+    end
+    redirect_to :back, flash: { error: 'Käyttäjätunnus tai sähköposti ei täsmää olemassaolevia käyttäjiä.' } and return
   end
 
   def update
+    redirect_to :back, flash: { success: 'Haista paska!' } and return
   end
+
+  private
+
+  def set_user_or_redirect
+    @user = User.find_by id: params[:id], reset_token: params[:reset_token]
+    if not @user
+      redirect_to :root and return
+    end
+    if @user.reset_sent_at < 2.hours.ago
+      redirect_to :root and return
+    end
+  end
+
 end
