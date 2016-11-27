d940bb8acd3405b444149437bd403992997e9eca
Lauri Vuojola <lauri.vuojola@gmail.com>
2016-11-27 19:22:49 +0200

    [fix] Ilmoittautumissivulle ohjaus oletuksena kirjautumisen jalkeen

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,7 +17,7 @@ class SessionsController < ApplicationController
           elsif params[:redirect] == 'event' and not params[:id].nil?
             redirect_to add_enrollment_path(event_id: params[:id]), flash: { success: "Tervetuloa " + user.first_name } and return
           else
-            redirect_to root_path, flash: { success: "Tervetuloa " + user.first_name } and return
+            redirect_to ilmoittautuminen_path, flash: { success: "Tervetuloa " + user.first_name } and return
           end
         else
           error_message = { error: 'Tunnus ei ole aktivoitu. Ohjeet aktivointiin on lähetetty sähköpostilla rekisteröitymisen yhteydessä.' }
